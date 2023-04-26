573ad64ddb97d4128e488336084c41d337b03894
jackf <jack.faghy@dataglide.co>
2023-04-26 15:24:48 +0100

    trial more stuff

--- Server/Controllers/WordModController.cs
+++ Server/Controllers/WordModController.cs
@@ -3,6 +3,8 @@ using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Paragraph = DocumentFormat.OpenXml.Wordprocessing.Paragraph;
+using Text = DocumentFormat.OpenXml.Wordprocessing.Text;
 
 namespace markdown2.Server.Controllers;
 
@@ -13,67 +15,91 @@ public class WordModController : ControllerBase
     [HttpPut]
     [Route("/word")]
     public IActionResult DownloadWord([FromBody] DownloadData downloadData) =>
+        //File(ReplaceDataTagsInWordDocument(downloadData.Column1, downloadData.Column2), "application/octet-stream", "jacks.docx");
         File(ModifyWordDoc(downloadData.Column1, downloadData.Column2), "application/octet-stream", "jacks.docx");
 
-    /*private byte[] ModifyWordDoc(string data, string data2)
+    public byte[]  ReplaceDataTagsInWordDocument(string data, string data2)
     {
-        byte[] byteArray = System.IO.File.ReadAllBytes("E:\\doc.docx");
-        using (var stream = new MemoryStream())
+        byte[] byteArray = System.IO.File.ReadAllBytes("C:\\DataGlide\\template.docx");
+        using (MemoryStream stream = new MemoryStream())
         {
             stream.Write(byteArray, 0, byteArray.Length);
-            using (var doc = WordprocessingDocument.Open(stream, true))
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(stream, true))
             {
-                // Get the main document part
-                MainDocumentPart mainPart = doc.MainDocumentPart;
+                /*// Find all text in the Word document
+                    IEnumerable<Text> textElements = wordDoc.MainDocumentPart.Document.Descendants<Text>();
+                    foreach (Text textElement in textElements)
+                    {
+                        // Check if the current text element contains the old data tag
+                        if (textElement.Text.Contains("<DATATAG1>"))
+                        {
+                            // Replace the old data tag with the new data tag
+                            textElement.Text = textElement.Text.Replace("<DATATAG1>", data);
+                        }
+                    }*/
+                
 
-                // Get the document text
-                string docText = mainPart.Document.Body.InnerText;
+                /*
+                foreach (Text textElement in textElements)
+                {
+                    // Check if the current text element contains the old data tag
+                    if (textElement.Text.Contains("<DATATAG1>"))
+                    {
+                        // Replace the old data tag with the new data tag
+                        textElement.Text = textElement.Text.Replace("<DATATAG1>", data);
+                    }
+                }*/
 
-                // Replace the data tags with JSON data
-                docText = docText.Replace("<DATATAG1>", JsonConvert.SerializeObject(data));
-                docText = docText.Replace("<DATATAG2>", JsonConvert.SerializeObject(data2));
+                // Find all content controls in the Word document
+                IEnumerable<SdtElement> contentControls = wordDoc.MainDocumentPart.Document.Descendants<SdtElement>();
 
-                // Update the document text
-                mainPart.Document.Body.RemoveAllChildren();
-                mainPart.Document.Body.Append(new Paragraph(new Run(new Text(docText))));
-
-                // Save the changes
-                mainPart.Document.Save(); // won't update the original file
+                foreach (SdtElement cc in contentControls)
+                {
+                    // Check if the current content control contains the old data tag
+                    Text textElement = cc.Descendants<Text>().FirstOrDefault();
+                    if (textElement != null && textElement.Text.Contains("<DATATAG1>"))
+                    {
+                        // Replace the old data tag with the new data tag
+                        textElement.Text = textElement.Text.Replace("<DATATAG1>", data);
+                    }
+                }
+                
+                // Save the updated Word document
+                wordDoc.MainDocumentPart.Document.Save();
             }
+
             // Save the file with the new name
             stream.Position = 0;
             //System.IO.File.WriteAllBytes("""C:\DataGlide\output.docx""", stream.ToArray());
             return stream.ToArray();
         }
-    }*/
+    }
 
     private byte[] ModifyWordDoc(string data, string data2)
     {
-        byte[] byteArray = System.IO.File.ReadAllBytes("E:\\doc.docx");
-        using (var stream = new MemoryStream())
+        byte[] byteArray = System.IO.File.ReadAllBytes("C:\\DataGlide\\templatecc.docx");
+        using (MemoryStream stream = new ())
         {
             stream.Write(byteArray, 0, byteArray.Length);
-            using (var doc = WordprocessingDocument.Open(stream, true))
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(stream, true))
             {
                 MainDocumentPart mainPart = doc.MainDocumentPart;
-                Body body = mainPart.Document.AppendChild(new Body());
 
-                // Copy the contents of the input document to the output document
-                foreach (var element in doc.MainDocumentPart.Document.Body.ChildElements)
+                foreach (OpenXmlElement element in mainPart.Document.Body.ChildElements)
                 {
-                    // Deep copy the element to preserve formatting
-                    //var cloneElement = element.CloneNode(true);
-
                     // Replace the data tags with JSON data in the cloned element
-                    foreach (var text in element.Descendants<Text>())
+                    if (element.GetType() == typeof(Paragraph))
                     {
-                        if (text.Text.Contains("<DATATAG1>"))
+                        foreach (Text text in element.Descendants<Text>())
                         {
-                            text.Text = text.Text.Replace("<DATATAG1>", JsonConvert.SerializeObject(data));
-                        }
-                        else if (text.Text.Contains("<DATATAG2>"))
-                        {
-                            text.Text = text.Text.Replace("<DATATAG2>", JsonConvert.SerializeObject(data2));
+                            if (text.Text.Contains("<DATA1>"))
+                            {
+                                text.Text = text.Text.Replace("<DATA1>", JsonConvert.SerializeObject(data));
+                            }
+                            else if (text.Text.Contains("<DATATAG2>"))
+                            {
+                                text.Text = text.Text.Replace("<DATATAG2>", JsonConvert.SerializeObject(data2));
+                            }
                         }
                     }
                 }
